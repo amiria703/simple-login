1e22452197763f30ae5b1c51d47b71dd47ecb3e3
amiria703 <amiria703@gmail.com>
2022-02-04 17:36:43 +0330

    intial

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,50 @@
 <template>
-    <div id="login">
-        <main>
-            <mq-layout mq="lg">
-                <SignUp/>
-            </mq-layout>
+  <div id="login">
+    <main>
+      <!-- Different styles for different devices: -->
+      <mq-layout mq="lg">
+        <Login />
+      </mq-layout>
 
-            <mq-layout mq="md">
-                <SignUp main="left:20%;width:60%;" input="width:40%;"/>
-            </mq-layout>
+      <mq-layout mq="md">
+        <Login mainStyle="left:20%;width:60%;" inputStyle="width:40%;" />
+      </mq-layout>
 
-            <mq-layout mq="sm">
-                <SignUp main="left:10%;width:80%;" input="width:50%;"/>
-            </mq-layout>
-        </main>
-    </div>
+      <mq-layout mq="sm">
+        <Login mainStyle="left:10%;width:80%;" inputStyle="width:50%;" />
+      </mq-layout>
+    </main>
+  </div>
 </template>
 
 <script>
-    import SignUp from './components/SignUp.vue'
+import Login from "./components/Login.vue";
 
-    export default {
-        name: 'App',
-        components: {
-            SignUp
-        }
-    }
+export default {
+  name: "App",
+  components: {
+    Login,
+  },
+  mounted: async () => {
+    //Animate being hidden after 2500ms and then remove after 500ms
+    await new Promise((r) => setTimeout(r, 2500));
+    document.getElementById("white").classList.add("hidden");
+    await new Promise((r) => setTimeout(r, 500));
+    document.getElementById("white").remove();
+  },
+};
 </script>
 
 <style>
-    #login {
-        background: url("assets/background.jpg") center center;
-        background-size: cover;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-    }
+#login {
+  background: url("assets/background.jpg") center center;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,109 @@
 <template>
-    <div class="main">
-        <h1>Sample Login Page</h1>
-        <h5>Made by amiria703</h5>
-    </div>
+  <div class="main" :style="mainStyle">
+    <h1>Welcome!</h1>
+    <input
+      type="text"
+      name="username"
+      id="username"
+      placeholder="User Name"
+      :style="input"
+    />
+    <br />
+    <input
+      type="password"
+      name="password"
+      id="password"
+      placeholder="P@$$W0RD"
+      :style="input"
+    />
+    <br />
+    <input
+      type="button"
+      value="Done!"
+      class="button"
+      id="done"
+      :style="inputStyle"
+    />
+    <br />
+    <img src="../assets/google.svg" alt="Login using Google" />
+    <img src="../assets/facebook.svg" alt="Login using Facebook" />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Login'
-    }
+export default {
+  name: "Login",
+  //Custom style for main and input for make the page responsive:
+  props: {
+    mainStyle: String,
+    inputStyle: String,
+  },
+};
 </script>
 
-<style scoped>
+<style>
+/* Import Poppins font: */
+@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
 
+.main {
+  background: rgba(255, 255, 255, 0.4);
+  position: absolute;
+  top: 20%;
+  left: 30%;
+  width: 40%;
+  text-align: center;
+  padding: 5px;
+  border-radius: 3rem;
+  box-shadow: 0px 0px 8px -5px #000000;
+  padding-top: 3%;
+  padding-bottom: 5%;
+  font-family: "Poppins", sans-serif;
+}
+
+h1 {
+  cursor: default;
+  user-select: none;
+}
+
+input {
+  border-radius: 3rem;
+  border: none;
+  padding: 10px;
+  text-align: center;
+  outline: none;
+  margin: 10px;
+  width: 30%;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+}
+
+input:hover {
+  box-shadow: 0px 0px 8px -5px #000000;
+}
+
+input:active {
+  box-shadow: 0px 0px 8px -5px #000000;
+}
+
+#done {
+  background: lightgreen;
+}
+
+.button {
+  cursor: pointer;
+  user-select: none;
+}
+
+img {
+  height: 2.2rem;
+  margin: 10px;
+  user-select: none;
+}
+
+img:hover {
+  box-shadow: 0px 0px 8px -5px #000000;
+  cursor: pointer;
+  border-radius: 200rem;
+}
 </style>
